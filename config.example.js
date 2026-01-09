295b407d2df0e55e96cbb4606ee71a717fd363a0
Yash Jain <06jainyash@gmail.com>
2026-01-10 04:42:33 +0530

    Update config.example.js

---
+++ config.example.js
@@ -0,0 +1,11 @@
+// config.example.js
+// ❗ DO NOT put real secrets here
+
+export const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
